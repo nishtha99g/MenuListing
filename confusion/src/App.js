8aee7e737f3d3c4b0e8eae10d60ea335fb9c6253
nishtha99 <nishtha99g@gmail.com>
2020-04-17 17:55:41 +0530

    React Components added

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent.js'
-
-function App() {
+import {DISHES} from './shared/dishes';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      dishes:DISHES,
+    };
+  }
+  render(){
   return (
     <div>
      <Navbar dark color="primary">
        <div className="container">
-         <NavbarBrand href="/">Confusion here</NavbarBrand>
+         <NavbarBrand href="/">Confusion here </NavbarBrand>
        </div>
      </Navbar>
-     <Menu />
+     <Menu dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
